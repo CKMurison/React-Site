271680833c8b74e6367ca2683743c6fce1d2404e
Cameron Murison <cmuris4@gmail.com>
2023-10-04 11:22:16 +0100

    added img to sidebar and scss

--- my-site/src/Components/Sidebar/index.js
+++ my-site/src/Components/Sidebar/index.js
@@ -1,5 +1,16 @@
-import "./index.scss"
+import { Link } from 'react-router-dom'
+import "./index.scss" 
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
 
-const Sidebar = () => <>Hello from the sidebar</>
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt="logo" />
+            <img src={LogoSubtitle} alt="Slob" />
+        </Link>
+    </div>
+)
 
 export default Sidebar
